874e547713afcd9024ff2aed5a9134eb9b68901f
SURIYALEARNING <editorsoflhcc@gmail.com>
2026-07-15 08:02:33 +0530

    un usage dependacys removed from frontend and backend in package.json

--- backend/database.js
+++ backend/database.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 const connectDB = async () => {
   try {
     // Replace with your actual URI string or use process.env.MONGO_URI
-    const conn = await mongoose.connect(process.env.MONGO_URI); 
+    const conn = await mongoose.connect(process.env.DATABASE_URL); 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Database connection error: ${error.message}`);
